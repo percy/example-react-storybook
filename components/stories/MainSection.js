f6385b3401cdeadba42c288a2e468bf7fd9528b5
Daniil Samoylov <daniil.samoylov@auror.co>
2018-10-05 14:13:06 +1300

    Upgrade to new storybook

--- components/stories/MainSection.js
+++ components/stories/MainSection.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MainSection from '../MainSection';
-import { storiesOf, action } from '@kadira/storybook';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 storiesOf('MainSection', module)
   .add('all active', () => {
--- components/stories/header.js
+++ components/stories/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../Header';
-import { storiesOf, action } from '@kadira/storybook';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 storiesOf('Header', module)
   .add('default view', () => {
